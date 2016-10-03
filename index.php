dc140e98f63ce986befbfe7fa7ab81d41e332db3
HarryNicholls <harry.nicholls@hotmail.com>
2016-10-03 15:22:40 +0100

    test

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <body>
 <p>
     <?php
+    /*
     echo "Hello World";
 
     $name="harry";
@@ -27,6 +28,39 @@
     else {
         print "Who are you fam?";
     }
+*/
+    $numberOfHobbits = 2;
+    switch ($numberOfHobbits) {
+        case 1:
+            echo "1 sad hobbit";
+            break;
+        case 2:
+            echo "2 happy hobbits";
+            break;
+        case 3:
+            echo "3 hobbits are a crowd";
+            break;
+        default:
+            echo "All the hobbits have gone home";
+    }
+
+
+    $myArray = array("do", "re", "mi"); // declares the array
+    echo $myArray[0]; // outputs "do"
+    $myArray[1] = "la"; // modifies position 1 (re)
+    echo $myArray[1];// outputs "la"
+    unset($myArray[2]); // removes the array in position 2
+
+
+    for ($i = 1; $i < 10; $i++)
+    {
+        echo "<p>Hello!</p>";
+    }
+/*
+    foreach($arrayelement as $x) {
+        print "<p>$x</p>";
+    }
+*/
     ?>
 </p>
 
