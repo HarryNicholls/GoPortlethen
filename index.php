b732806290aa860269037e954d707ea63c2b790b
HarryNicholls <harry.nicholls@hotmail.com>
2016-10-03 14:46:17 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello world</title> //sets the title
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+
+    $name="harry";
+
+    if ($name == "simon") {
+        print "I know you!";
+    }
+    else {
+        print "Who are you fam?";
+    }
+    ?>
+</p>
+
+</body>
+</html>
